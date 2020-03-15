9eebee2e2e551237fa1d77fa60d0f4c2d432869a
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-03-16 00:20:19 +0545

    Menu styled component

--- src/components/MenuSelect/index.tsx
+++ src/components/MenuSelect/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from './styles.module.scss';
+import styled from 'styled-components';
 
 type Props = {
   clickHandler: any,
@@ -12,22 +12,40 @@ type Props = {
   }>
 }
 
+const Ul = styled.ul`
+  list-style-type: none;
+  font-size: 1.3rem;
+`;
+
+interface LiProps {
+  readonly selected: boolean;
+};
+
+const Li = styled.li<LiProps>`
+    padding: 5px 40px;
+    background: ${props => props.selected ? 'silver' : 'auto'};
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+`;
+
 function MenuSelect(props: Props) {
   const { clickHandler, selectedChoice, choices } = props;
   return (
-    <ul className={styles['menu-container']}>
+    <Ul>
       {
         choices.map(choice => (
-          <li
+          <Li
             key={choice.id}
-            className={choice.id === selectedChoice ? styles['selected'] : undefined}
+            selected={choice.id === selectedChoice}
             onClick={() => clickHandler(choice.id)}
           >
             <a href={`#${choice.path}`}>{choice.label}</a>
-          </li>
+          </Li>
         ))
       }
-    </ul>
+    </Ul>
   );
 };
 
